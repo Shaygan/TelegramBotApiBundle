7909566a23b6dec0161031ae3d20ef809506464f
Evgeniy Zatsepin <7dizzy7@gmail.com>
2015-12-15 21:02:35 +0300

    fixes in sample receiver

--- UpdateReceiver/UpdateReceiver.php
+++ UpdateReceiver/UpdateReceiver.php
@@ -2,8 +2,9 @@
 
 namespace Shaygan\TelegramBotApiBundle\UpdateReceiver;
 
+use Shaygan\TelegramBotApiBundle\TelegramBotApi;
 use Shaygan\TelegramBotApiBundle\Type\Update;
-use Symfony\Component\DependencyInjection\Container;
+use TelegramBot\Api\Types\Chat;
 
 /**
  *
@@ -12,35 +13,34 @@ use Symfony\Component\DependencyInjection\Container;
 class UpdateReceiver implements UpdateReceiverInterface
 {
 
-    private $container;
+    private $config;
+    private $telegramBotApi;
 
-    public function __construct(Container $container)
+    public function __construct(TelegramBotApi $telegramBotApi, $config)
     {
-        $this->container = $container;
+        $this->telegramBotApi = $telegramBotApi;
+        $this->config = $config;
     }
 
     public function handleUpdate(Update $update)
     {
-        $cmd = $update->message->chat;
-        $config = $this->container->getParameter("shaygan_telegram_bot_api.config");
+        $cmd = $update->message->getChat();
 
         switch ($cmd) {
             case "/about":
-            case "/about@{$config['bot_name']}":
+            case "/about@{$this->config['bot_name']}":
                 $text = "I'm a samble Telegram Bot";
                 break;
             case "/help":
-            case "/help@{$config['bot_name']}":
+            case "/help@{$this->config['bot_name']}":
             default :
                 $text = "Command List:\n";
                 $text .= "/about - About this bot\n";
                 $text .= "/help - show this help message\n";
                 break;
         }
-        $this->container->get("shaygan.telegram_bot_api")
-                ->sendMessage(
-                        $update->message->chat->id, $text
-        );
+        /** @var Chat $chat */
+        $chat = $update->message->getChat();
+        $this->telegramBotApi->sendMessage($chat->getId(), $text);
     }
-
 }
